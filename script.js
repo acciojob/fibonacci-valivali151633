9423e8416f1d315bd1a3a33d66c5de24b9a9b561
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-12-01 09:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+// function fibonacci(num) {
+// // your code here
+// 	if (num === 1) return 0;
+// 	if (num === 2) return 1;
+// 	return fibonacci(num-1)+fibonacci(num-2);
+// }
 function fibonacci(num) {
-// your code here
-	if (num === 1) return 0;
-	if (num === 2) return 1;
-	return fibonacci(num-1)+fibonacci(num-2);
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let a = 0, b = 1;
+    for (let i = 2; i < num; i++) {
+      let temp = b;
+      b = a + b;
+      a = temp;
+    }
+    return b;
+  }
 }
 
 module.exports = fibonacci;
